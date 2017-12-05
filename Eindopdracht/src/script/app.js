2a8368bb92f024b6c46dc2c30b81119d8ba48611
elien <elien.knockaert@student.howest.be>
2017-12-05 20:20:02 +0100

    css aanpassingen + score

--- Eindopdracht/src/script/app.js
+++ Eindopdracht/src/script/app.js
@@ -10,66 +10,117 @@
 // var url_vehicles = "https://opentdb.com/api.php?amount=4&category=28&difficulty=easy&type=boolean";
 // var url_history = "https://opentdb.com/api.php?amount=4&category=23&difficulty=easy&type=boolean";
 // var url_film = "https://opentdb.com/api.php?amount=4&category=11&difficulty=easy&type=boolean";
+var question1 = undefined;
+var answer0 = undefined;
+var score = 0;
+var showScore = undefined;
+
+function initVars(){
+    question1 = document.getElementById("question1");
+    answer0 = document.getElementById("answer");
+    showScore = document.getElementById("score");
+}
 
 
-var question1 = document.getElementById("question1");
-var answer0 = document.getElementById("answer");
+var currentQuestion, currentAnswer;
 
 function QuestionPageGeneral(functie) {
     var url_generalKnowledge = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean";
     var result2 = jQuery.getJSON(url_generalKnowledge, functie);
     return result2;
 }
+function nextQuestion(){
+    QuestionPageGeneral(function (data) {
+        data = data.results[0];
+        currentQuestion = data.question;
+        currentAnswer = data.correct_answer;
+    
+        showQuestion();
+        showAnswerDebug();
+    })
+}
 
 function QuestionPageArt(functie) {
-    var url_generalKnowledge = "https://opentdb.com/api.php?amount=4&category=25&difficulty=easy&type=boolean";
+    var url_generalKnowledge = "https://opentdb.com/api.php?amount=1&category=25&difficulty=easy&type=boolean";
     var result2 = jQuery.getJSON(url_generalKnowledge, functie);
     return result2;
 }
-
-QuestionPageGeneral(question)
-QuestionPageGeneral(answerOnScreen)
-//QuestionPageArt(question)
-
-function question(data) {
-    console.log(data);
-    var result_question = data.results[0].question;
-
-    question1.innerHTML = "" + result_question;
+function nextQuestion(){
+    QuestionPageArt(function (data) {
+        data = data.results[0];
+        currentQuestion = data.question;
+        currentAnswer = data.correct_answer;
+    
+        showQuestion();
+        showAnswerDebug();
+    })
 }
 
-function answerOnScreen(data)
+function QuestionPageArt(functie) {
+    var url_generalKnowledge = "https://opentdb.com/api.php?amount=4&category=11&difficulty=easy&type=boolean";
+    var result2 = jQuery.getJSON(url_generalKnowledge, functie);
+    return result2;
+}
+function nextQuestion(){
+    QuestionPageArt(function (data) {
+        data = data.results[0];
+        currentQuestion = data.question;
+        currentAnswer = data.correct_answer;
+    
+        showQuestion();
+        //showAnswerDebug();
+    })
+}
+
+
+
+function showQuestion(data) {
+    question1.innerHTML = "" + currentQuestion;
+}
+
+function scoreOnDisplay(data){
+    showScore.innerHTML = "Score: " + score;
+}
+
+function showAnswerDebug(data)
 {
-    var answer = data.results[0].correct_answer;
-    if (answer == "True") {
-        console.log("answer: Yes");
-        var answer = data.results[0].correct_answer;
+    if (currentAnswer == "True") {
         answer0.innerHTML = "answer: Yes";
     }
     else {
-        console.log("answer: No");
-        var answer = data.results[0].correct_answer;
         answer0.innerHTML = "answer: No";
     }
 }
 
+document.addEventListener('DOMContentLoaded', function(){
+    initVars();
+    nextQuestion();
+    scoreOnDisplay();
+
+    var YesButton = document.querySelector('.YesButton');
+    YesButton.addEventListener('click', function(e){e.preventDefault();
+        if (currentAnswer == "True"){
+            score ++;
+            console.log(score);
+        }
+        
+        nextQuestion();
+        scoreOnDisplay();
+    });
+
+    var NoButton = document.querySelector('.NoButton');
+    NoButton.addEventListener('click', function(e){e.preventDefault();
+        if (currentAnswer == "False"){
+            score ++;
+            console.log(score);
+        }
+        
+        nextQuestion();
+        scoreOnDisplay();
+    })
+})
 
 
-function ButtonNoClicked(data)
-{
-    console.log(data);
-    var result_question1 = data.results[0].question;
-    console.log("answer: Yes");
-    // var answer = QuestionPageGeneral() data.results[i].correct_answer;
-    alert(QuestionPageGeneral());
-}​
-
-// function ButtonYesClicked(data)
-// {
-//     console.log("answer: Yes");
-//     // var answer = QuestionPageGeneral() data.results[i].correct_answer;
-//     alert(QuestionPageGeneral());
-// }​
 
     
 
