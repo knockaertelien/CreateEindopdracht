016206d13299ae8cd066efce4f16477a4179bda4
elien <elien.knockaert@student.howest.be>
2017-12-08 11:30:14 +0100

    almost finished

--- Eindopdracht/src/script/app.js
+++ Eindopdracht/src/script/app.js
@@ -1,127 +1,157 @@
 'use strict';
 
-// var url_generalKnowledge = "https://opentdb.com/api.php?amount=4&category=9&difficulty=easy&type=boolean";
-// var url_scienceAndNature = "https://opentdb.com/api.php?amount=3&category=17&difficulty=easy&type=boolean";
-// var url_computers = "https://opentdb.com/api.php?amount=4&category=18&difficulty=easy&type=boolean";
-// var url_mathematics = "https://opentdb.com/api.php?amount=4&category=19&difficulty=easy&type=boolean";
-// var url_sports = "https://opentdb.com/api.php?amount=4&category=21&difficulty=easy&type=boolean";
-// var url_geography = "https://opentdb.com/api.php?amount=4&category=22&difficulty=easy&type=boolean";
-// var url_art = "https://opentdb.com/api.php?amount=4&category=25&difficulty=easy&type=boolean";
-// var url_vehicles = "https://opentdb.com/api.php?amount=4&category=28&difficulty=easy&type=boolean";
-// var url_history = "https://opentdb.com/api.php?amount=4&category=23&difficulty=easy&type=boolean";
-// var url_film = "https://opentdb.com/api.php?amount=4&category=11&difficulty=easy&type=boolean";
-var question1 = undefined;
-var answer0 = undefined;
-var score = 0;
-var showScore = undefined;
+var category = undefined,currentQuestion = undefined, currentAnswer = undefined, question1 = undefined, answer0 = undefined, score = 0, showScore = undefined, scoreIncorrect = undefined, scoreCorrect = undefined, showVictoryContent = undefined, hideVictoryContent = undefined, showGeneralContent = undefined, hideGeneralContent = undefined;
 
-function initVars(){
+var categories = [{
+    id: "9",
+    url: "https://opentdb.com/api.php?amount=1&category=9&type=boolean",
+    name: "General"
+},{
+    id: "17",
+    url: "https://opentdb.com/api.php?amount=1&category=17&type=boolean",
+    name: "Science"
+},{
+    id: "18",
+    url: "https://opentdb.com/api.php?amount=1&category=18&type=boolean",
+    name: "Computers"
+},{
+    id: "19",
+    url: "https://opentdb.com/api.php?amount=1&category=19&type=boolean",
+    name: "Mathematics"
+},{
+    id: "21",
+    url: "https://opentdb.com/api.php?amount=1&category=21&type=boolean",
+    name: "Sports"
+},{
+    id: "22",
+    url: "https://opentdb.com/api.php?amount=1&category=22&type=boolean",
+    name: "Geography"
+},{
+    id: "25",
+    url: "https://opentdb.com/api.php?amount=1&category=25&type=boolean",
+    name: "Art"
+},{
+    id: "28",
+    url: "https://opentdb.com/api.php?amount=1&category=28&type=boolean",
+    name: "Vehicles"
+},{
+    id: "23",
+    url: "https://opentdb.com/api.php?amount=1&category=23&type=boolean",
+    name: "History"
+},{
+    id: "11",
+    url: "https://opentdb.com/api.php?amount=1&category=11&type=boolean",
+    name: "Movies"
+},]
+
+function initVars() {
     question1 = document.getElementById("question1");
     answer0 = document.getElementById("answer");
     showScore = document.getElementById("score");
+    scoreCorrect = document.getElementById("scoreCorrect");
+    scoreIncorrect = document.getElementById("scoreIncorrect");
+    hideGeneralContent = document.getElementById("general_div");
+    showGeneralContent = document.getElementById("general_div");
+    hideVictoryContent = document.getElementById("victoryyy");
+    showVictoryContent = document.getElementById("victoryyy");
+    category = getCategory();
 }
 
-
-var currentQuestion, currentAnswer;
-
-function QuestionPageGeneral(functie) {
-    var url_generalKnowledge = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean";
-    var result2 = jQuery.getJSON(url_generalKnowledge, functie);
-    return result2;
+function getCategory() {
+	var query = window.location.href.split("cat=")[1];
+	for (var cat in categories){
+		if (categories[cat].id === query)
+			return categories[cat].url;
+	}
+	return null;
 }
-function nextQuestion(){
-    QuestionPageGeneral(function (data) {
+
+function QuestionPage(functie) {
+    var url = category;
+    var result = jQuery.getJSON(url, functie);
+    return result;
+    console.log(result);
+}
+
+function nextQuestion() {
+    QuestionPage(function (data) {
         data = data.results[0];
         currentQuestion = data.question;
         currentAnswer = data.correct_answer;
-    
         showQuestion();
-        showAnswerDebug();
-    })
-}
-
-function QuestionPageArt(functie) {
-    var url_generalKnowledge = "https://opentdb.com/api.php?amount=1&category=25&difficulty=easy&type=boolean";
-    var result2 = jQuery.getJSON(url_generalKnowledge, functie);
-    return result2;
-}
-function nextQuestion(){
-    QuestionPageArt(function (data) {
-        data = data.results[0];
-        currentQuestion = data.question;
-        currentAnswer = data.correct_answer;
-    
-        showQuestion();
-        showAnswerDebug();
-    })
-}
-
-function QuestionPageArt(functie) {
-    var url_generalKnowledge = "https://opentdb.com/api.php?amount=4&category=11&difficulty=easy&type=boolean";
-    var result2 = jQuery.getJSON(url_generalKnowledge, functie);
-    return result2;
-}
-function nextQuestion(){
-    QuestionPageArt(function (data) {
-        data = data.results[0];
-        currentQuestion = data.question;
-        currentAnswer = data.correct_answer;
-    
-        showQuestion();
-        //showAnswerDebug();
     })
 }
 
 
+
+function controleScore(){
+    if (score == 10){
+        showVictory();
+    }
+}
+
+function showVictory(){
+    showVictoryContent.style.display = "flex";
+    showVictoryContent.style.alignItems = "center";
+}
 
 function showQuestion(data) {
     question1.innerHTML = "" + currentQuestion;
 }
 
-function scoreOnDisplay(data){
+function scoreOnDisplay(data) {
     showScore.innerHTML = "Score: " + score;
 }
 
-function showAnswerDebug(data)
-{
-    if (currentAnswer == "True") {
-        answer0.innerHTML = "answer: Yes";
-    }
-    else {
-        answer0.innerHTML = "answer: No";
-    }
+function correct(data) {
+    scoreCorrect.innerHTML = "THAT IS CORRECT! <br>KEEP UP THE GOOD WORK!";
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+function notCorrect(data) {
+    scoreCorrect.innerHTML = "THAT IS INCORRECT ... <br>TRY HARDER!";
+}
+
+function changeScore(state){
+    score ++;
+    controleScore();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     initVars();
     nextQuestion();
     scoreOnDisplay();
 
+    
+
+    if (category === null){
+        return window.location = "index.html";
+    }
+
     var YesButton = document.querySelector('.YesButton');
-    YesButton.addEventListener('click', function(e){e.preventDefault();
-        if (currentAnswer == "True"){
-            score ++;
-            console.log(score);
+    YesButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (currentAnswer == "True") {
+            correct();
+            changeScore();
         }
-        
+        else {
+            notCorrect();
+        }
         nextQuestion();
         scoreOnDisplay();
     });
 
     var NoButton = document.querySelector('.NoButton');
-    NoButton.addEventListener('click', function(e){e.preventDefault();
-        if (currentAnswer == "False"){
-            score ++;
-            console.log(score);
+    NoButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (currentAnswer == "False") {
+            correct();
+            changeScore();
         }
-        
+        else {
+            notCorrect();
+        }
         nextQuestion();
         scoreOnDisplay();
     })
 })
-
-
-
-    
-
-
